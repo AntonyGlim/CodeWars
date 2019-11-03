ce0c7e43809d61dc02b10619c237aabf1a29410c
Antony.Glim <Antony.Glim@gmail.com>
2019-11-03 22:45:35 +0300

    In process KataFirstVariationOnCaesarCipher
    Just one bug with a except z

--- src/main/java/glim/antony/katas/kata5/KataFirstVariationOnCaesarCipher.java
+++ src/main/java/glim/antony/katas/kata5/KataFirstVariationOnCaesarCipher.java
@@ -8,34 +8,34 @@ import java.util.stream.Stream;
 
 /**
  * https://www.codewars.com/kata/first-variation-on-caesar-cipher/train/java
- *
+ * <p>
  * The action of a Caesar cipher is to replace each plaintext letter with a different
  * one a fixed number of places up or down the alphabet.
- *
+ * <p>
  * This program performs a variation of the Caesar shift.
  * The shift increases by 1 for each character (on each iteration).
- *
+ * <p>
  * If the shift is initially 1, the first character of the message
  * to be encoded will be shifted by 1, the second character will be shifted by 2, etc...
- *
+ * <p>
  * Coding: Parameters and return of function "movingShift"
  * param s: a string to be coded
- *
+ * <p>
  * param shift: an integer giving the initial shift
- *
+ * <p>
  * The function "movingShift" first codes the entire string
  * and then returns an array of strings containing
  * the coded string in 5 parts (five parts because,
  * to avoid more risks, the coded message will be given
  * to five runners, one piece for each runner).
- *
+ * <p>
  * If possible the message will be equally divided by message length
  * between the five runners. If this is not possible,
  * parts 1 to 5 will have subsequently non-increasing lengths,
  * such that parts 1 to 4 are at least as long as when evenly divided,
  * but at most 1 longer. If the last part is the empty string
  * this empty string must be shown in the resulting array.
- *
+ * <p>
  * For example, if the coded message has a length of 17
  * the five parts will have lengths of 4, 4, 4, 4, 1.
  * The parts 1, 2, 3, 4 are evenly split
@@ -46,74 +46,108 @@ import java.util.stream.Stream;
  * since his part is the empty string.
  * If the length is 11, equal parts would be of length 2.2,
  * hence parts will be of lengths 3, 3, 3, 2, 0.
- *
+ * <p>
  * You will also implement a "demovingShift" function with two parameters
- *
+ * <p>
  * Decoding: parameters and return of function "demovingShift"
  * 1) an array of strings: s (possibly resulting from "movingShift", with 5 strings)
- *
+ * <p>
  * 2) an int shift
- *
+ * <p>
  * "demovingShift" returns a string.
- *
+ * <p>
  * Example:
  * u = "I should have known that you would have a perfect answer for me!!!"
- *
+ * <p>
  * movingShift(u, 1) returns :
- *
+ * <p>
  * v = ["J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!"]
- *
+ * <p>
  * (quotes added in order to see the strings and the spaces, your program won't write these quotes, see Example Test Cases)
- *
+ * <p>
  * and demovingShift(v, 1) returns u.
- *
+ * <p>
  * #Ref:
- *
+ * <p>
  * Caesar Cipher : http://en.wikipedia.org/wiki/Caesar_cipher
  */
 public class KataFirstVariationOnCaesarCipher {
-    public static List<String>  movingShift(String s, int shift) {
+    public static List<String> movingShift(String s, int shift) {
         char[] chars = s.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            if ((chars[i] >= 65 && chars[i] <= 90) || (chars[i] >= 97 && chars[i] <= 122)){
-                chars[i] = (char) (chars[i] + shift % 26 - 26 * (shift / 26));
-            }
-            shift++;
+            chars[i] = shift(chars[i], shift++);
         }
-        System.out.println(Arrays.toString(chars));
-        return new ArrayList<>();
+        return splitUpString(new String(chars));
     }
 
-    public static String  demovingShift(List<String> s, int shift) {
-		return "";
+    public static String demovingShift(List<String> s, int shift) {
+        String list = String.join("", s);
+        char[] chars = list.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = deShift(chars[i], shift++);
+        }
+        return new String(chars);
     }
 
-//    public static char[] alphabet() {
-////        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-//        return "abcdefghijklmnopqrstuvwxyz".toCharArray();
-//    }
-
-    public static void characterNumbers(){
-        System.out.println("a: " + (int) new Character('a'));
-        System.out.println("z: " + (int) new Character('z'));
-        System.out.println("A: " + (int) new Character('A'));
-        System.out.println("Z: " + (int) new Character('Z'));
-        System.out.println("{: " + (int) new Character('{'));
+    public static Character shift(Character ch, int shift) {
+        if (ch >= 65 && ch <= 90) { //A-Z
+            return (ch - 64 + shift) % 26 == 0 ? 90 : (char) (64 + (ch - 64 + shift) % 26);
+        }
+        if (ch >= 97 && ch <= 122) { //a-z
+            return (ch - 96 + shift) % 26 == 0 ? 122 : (char) (96 + (ch - 96 + shift) % 26);
+        }
+        return ch;
     }
 
-    public static Character formula(Character ch, int shift){
-        return (char) (96 + (ch - 96 + shift) % 26);
+    public static Character deShift(Character ch, int shift) {
+        if (ch >= 65 && ch <= 90) { //A-Z
+            return (ch - 64 - shift) % 26 == 0 ? 65 : (char) (89 + (ch - 89 - shift) % 26);
+        }
+        if (ch >= 97 && ch <= 122) { //a-z
+            return (ch - 96 - shift) % 26 == 0 ? 97 : (char) (121 + (ch - 121 - shift) % 26);
+        }
+        return ch;
+    }
+
+    public static int[] findPartsSize(int stringLength) {
+        int[] partsSize = new int[5];
+        if (stringLength % 5 == 0) {
+            Arrays.fill(partsSize, stringLength / 5);
+            return partsSize;
+        }
+        for (int i = 5; i > 0; i--) {
+            if (stringLength % i < stringLength / i && i != 5) {
+                for (int j = 0; j < i; j++) {
+                    partsSize[j] = stringLength / i;
+                }
+                partsSize[i] = stringLength % i;
+                for (int j = partsSize[0]; j > partsSize[4]; j--) {
+                    for (int k = 0; k < 4; k++) {
+                        partsSize[k]--;
+                    }
+                    partsSize[4] += i;
+                }
+                for (int k = 0; k < 4; k++) {
+                    partsSize[k]++;
+                }
+                partsSize[4] -= i;
+                return partsSize;
+            }
+        }
+        return partsSize;
+    }
+
+    public static List<String> splitUpString(String s) {
+        List<String> strings = new ArrayList<>();
+        int[] parts = findPartsSize(s.length());
+        int beginIndex = 0;
+        int endIndex = parts[0];
+
+        for (int i : parts) {
+            strings.add(s.substring(beginIndex, endIndex));
+            beginIndex = endIndex;
+            endIndex = Math.min(beginIndex + i, s.length());
+        }
+        return strings;
     }
 }
-
-//	lcA = "abcdefghijklmnopqrstuvwxyz"
-//	ucA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-//    char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-
-
-//a: 97
-//z: 122
-
-//A: 65
-//Z: 90
--- src/test/java/glim/antony/katas/kata5/KataFirstVariationOnCaesarCipherTest.java
+++ src/test/java/glim/antony/katas/kata5/KataFirstVariationOnCaesarCipherTest.java
@@ -10,10 +10,6 @@ import static org.junit.Assert.*;
 public class KataFirstVariationOnCaesarCipherTest {
 
     @Test
-    public void movingShift() {
-    }
-
-    @Test
     public void demovingShift() {
     }
 
@@ -21,24 +17,77 @@ public class KataFirstVariationOnCaesarCipherTest {
     public void test1() {
         String u = "I should have known that you would have a perfect answer for me!!!";
         List<String> v = Arrays.asList("J vltasl rlhr ", "zdfog odxr ypw", " atasl rlhr p ", "gwkzzyq zntyhv", " lvz wp!!!");
+        assertEquals(u, KataFirstVariationOnCaesarCipher.demovingShift(KataFirstVariationOnCaesarCipher.movingShift(u, 1), 1));
         assertEquals(v, KataFirstVariationOnCaesarCipher.movingShift(u, 1));
+    }
+    @Test
+    public void test2() {
+        String u = "O CAPTAIN! my Captain! our fearful trip is done;";
         assertEquals(u, KataFirstVariationOnCaesarCipher.demovingShift(KataFirstVariationOnCaesarCipher.movingShift(u, 1), 1));
     }
-
     @Test
-    public void characterNumbers() {
-        KataFirstVariationOnCaesarCipher.characterNumbers();
+    public void test3() {
+        String u = "For you bouquets and ribbon'd wreaths--for you the shores a-crowding;";
+        assertEquals(u, KataFirstVariationOnCaesarCipher.demovingShift(KataFirstVariationOnCaesarCipher.movingShift(u, 1), 1));
+    }
+    @Test
+    public void test4() {
+        String u = "Exult, O shores, and ring, O bells! But I, with mournful tread, Walk the deck my Captain lies, Fallen cold and dead. ";
+        assertEquals(u, KataFirstVariationOnCaesarCipher.demovingShift(KataFirstVariationOnCaesarCipher.movingShift(u, 1), 1));
+    }
+    @Test
+    public void testa() {
+        String u = " uoxIirmoveNreefckgieaoiEcooqo";
+        List<String> v = Arrays.asList(" xscOp", "zvygqA", "ftuwud", "adaxmh", "Edqrut");
+        assertEquals(u, KataFirstVariationOnCaesarCipher.demovingShift(KataFirstVariationOnCaesarCipher.movingShift(u, 2), 2));
+        assertEquals(v, KataFirstVariationOnCaesarCipher.movingShift(u, 2));
+    }
+    @Test
+    public void testb() {
+        String u = "uaoQop jx eh osr okaKv vzagzwpxagokBKriipmc U";
+        List<String> v = Arrays.asList("wdsVuw sh", " qu dii h", "evGs uzbi", "caudhoxuM", "Wewxfdu O");
+        assertEquals(u, KataFirstVariationOnCaesarCipher.demovingShift(KataFirstVariationOnCaesarCipher.movingShift(u, 2), 2));
+        assertEquals(v, KataFirstVariationOnCaesarCipher.movingShift(u, 2));
+    }
+    @Test
+    public void testc() {
+        String u = "kgpiqislyhvmffdzlyehjiIteAaaotcoapk bbMgaHlda";
+        List<String> v = Arrays.asList("mjtnwpaui", "shztutqdr", "ycffGseBc", "dsyiviyu ", "noAvqYdwu");
+        assertEquals(u, KataFirstVariationOnCaesarCipher.demovingShift(KataFirstVariationOnCaesarCipher.movingShift(u, 2), 2));
+        assertEquals(v, KataFirstVariationOnCaesarCipher.movingShift(u, 2));
+    }
+    @Test
+    public void testd() {
+        String u = "abcdefghjuty";
+        List<String> v = Arrays.asList("bdf", "hjl", "nps", "eek", "");
+        assertEquals(u, KataFirstVariationOnCaesarCipher.demovingShift(KataFirstVariationOnCaesarCipher.movingShift(u, 1), 1));
+        assertEquals(v, KataFirstVariationOnCaesarCipher.movingShift(u, 1));
+    }
+    @Test
+    public void teste() {
+        String u = "abcdefghjuty1234";
+        List<String> v = Arrays.asList("bdfh", "jlnp", "seek", "1234", "");
+        assertEquals(u, KataFirstVariationOnCaesarCipher.demovingShift(KataFirstVariationOnCaesarCipher.movingShift(u, 1), 1));
+        assertEquals(v, KataFirstVariationOnCaesarCipher.movingShift(u, 1));
     }
 
     @Test
-    public void formula() {
-        assertTrue('c'== KataFirstVariationOnCaesarCipher.formula('a', 2));
-        assertTrue('b'== KataFirstVariationOnCaesarCipher.formula('z', 2));
-        assertTrue('C'== KataFirstVariationOnCaesarCipher.formula('A', 2));
-        assertTrue('B'== KataFirstVariationOnCaesarCipher.formula('Z', 2));
+    public void shift() {
+        assertEquals('c', (char) KataFirstVariationOnCaesarCipher.shift('a', 2));
+        assertEquals('a', (char) KataFirstVariationOnCaesarCipher.shift('c', -2));
+        assertEquals('z', (char) KataFirstVariationOnCaesarCipher.shift('z', 0));
+        assertEquals('C', (char) KataFirstVariationOnCaesarCipher.shift('A', 2));
+        assertEquals('B', (char) KataFirstVariationOnCaesarCipher.shift('Z', 2));
+        assertEquals('(', (char) KataFirstVariationOnCaesarCipher.shift('(', 2));
+        assertEquals('u', (char) KataFirstVariationOnCaesarCipher.deShift('a', 6));
+    }
+
+    @Test
+    public void findPartsSize() {
+        assertArrayEquals(new int[] {3, 3, 3, 3, 3}, KataFirstVariationOnCaesarCipher.findPartsSize(15));
+        assertArrayEquals(new int[] {4, 4, 4, 4, 1}, KataFirstVariationOnCaesarCipher.findPartsSize(17));
+        assertArrayEquals(new int[] {3, 3, 3, 2, 0}, KataFirstVariationOnCaesarCipher.findPartsSize(11));
+        assertArrayEquals(new int[] {4, 4, 4, 4, 0}, KataFirstVariationOnCaesarCipher.findPartsSize(16));
+        assertArrayEquals(new int[] {14, 14, 14, 14, 10}, KataFirstVariationOnCaesarCipher.findPartsSize(66));
     }
 }
-
-
-//	lcA = "abcdefghijklmnopqrstuvwxyz"
-//	ucA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
